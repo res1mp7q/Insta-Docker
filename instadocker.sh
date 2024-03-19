f171d971fa31f0f4298f7082947f84e491876448
Jimmy Lamont <jimmy@lamont.family>
2024-03-18 22:35:39 -0700

    Initial push

---
+++ instadocker.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Excelsior INSTA-DOCKER Script January 2024
+
+export TERM=xterm-256color
+echo "$(tput setaf 45)                     ____ _  _ ____ ____ _    ____ _ ____ ____ "
+echo "$(tput setaf 45)                     |___  \/  |    |___ |    [__  | |  | |__/ "
+echo "$(tput setaf 45)                     |___ _/\_ |___ |___ |___ ___] | |__| |  \ "
+echo "$(tput setaf 7)"
+echo "$(tput setaf 7)  **************************************************************************************"
+echo "$(tput setaf 7)  *                          NEAR INSTANT DOCKER SUITE                                 *"
+echo "$(tput setaf 7)  **************************************************************************************"
+echo "$(tput setaf 7)                        Debian INSTA-DOCKER Script March 2024                           "
+echo ""
+# Add Docker's official GPG key:
+echo "Hey $USER, let's get started. Update, install prereqs, and keyrings"
+
+
+sudo apt-get update
+function loading_icon() {
+    local load_interval="${1}"
+    local loading_message="${2}"
+    local elapsed=0
+    local loading_animation=( ▰▱▱▱▱▱▱ ▰▰▱▱▱▱▱ ▰▰▰▱▱▱▱ ▱▰▰▰▱▱▱ ▱▱▰▰▰▱▱ ▱▱▱▰▰▰▱ ▱▱▱▱▰▰▰ ▱▱▱▱▱▰▰ ▱▱▱▱▱▱▰ ▱▱▱▱▱▱▱ ▱▱▱▱▱▱▱ ▱▱▱▱▱▱▱ ▱▱▱▱▱▱▱ )
+
+    echo -n "${loading_message} "
+
+    # This part is to make the cursor not blink
+    # on top of the animation while it lasts
+    tput civis
+    trap "tput cnorm" EXIT
+    while [ "${load_interval}" -ne "${elapsed}" ]; do
+        for frame in "${loading_animation[@]}" ; do
+            printf "%s\b" "${frame}"
+            sleep 0.25
+        done
+        elapsed=$(( elapsed + 1 ))
+    done
+    printf " \b\n"
+}
+
+loading_icon 60 "Updating"
+
+
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "Add the repository to Apt sources:"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+echo "And finally, install docker"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+
